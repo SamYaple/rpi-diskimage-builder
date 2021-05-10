c908eb132f506703d7c8f095f576e7f9998b51ca
Sam Yaple <sam+github@yaple.net>
2021-05-10 18:02:43 +0000

    refactor build script

--- build.sh
+++ build.sh
@@ -9,26 +9,23 @@ set -eux
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 VENV_DIR="${SCRIPT_DIR}/dib-venv"
 
-# http_proxy pointed to apt-cacher-ng server
+if [[ ! -x "$(command -v disk-image-create)" ]]; then
+    if [[ ! -d "${VENV_DIR}" ]]; then
+        python3 -m venv "${VENV_DIR}"
+        source "${VENV_DIR}/bin/activate"
+        pip install -U diskimage-builder
+        deactivate
+    fi
+    source "${VENV_DIR}/bin/activate"
+fi
+
 export http_proxy=http://192.168.0.254:3142
-
-# $DIB_CREATE_USERNAME will be created and added to the sudo group
 export DIB_CREATE_USERNAME=mgmt
-# $DIB_ADD_AUTHORIZED_SSH_KEY is added to /home/$DIB_CREATE_USERNAME/.ssh/authorized_keys
 export DIB_ADD_AUTHORIZED_SSH_KEY="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK8/vEPvz4foBsFFi1i2mBrf8ZEpiYFtt7VjCLfwgO4R"
-
 export DIB_DEBIAN_COMPONENTS=main,universe,multiverse,restricted
 export DIB_RELEASE=hirsute
 export DIB_UBUNTU_KERNEL=linux-raspi
 export ELEMENTS_PATH="${SCRIPT_DIR}/elements"
 
-if [[ ! -d "${VENV_DIR}" ]]; then
-    python3 -m venv "${VENV_DIR}"
-    source "${VENV_DIR}/bin/activate"
-    pip install -U diskimage-builder
-    deactivate
-fi
-
-source "${VENV_DIR}/bin/activate"
 disk-image-create -t qcow2 -o ~/rpi4.qcow2 rpi4
 # qemu-img convert -f qcow2 -O raw ~/rpi4.qcow2 /dev/mmcblk1
